e01e7d5aa5b8d54b227919b51fca659736a0a793
Danila Buryakov <danila.buryakovv@mail.ru>
2025-06-17 21:07:46 +0300

    modified:   tableshop/main/__pycache__/models.cpython-313.pyc 	new file:   tableshop/main/migrations/0001_initial.py 	new file:   tableshop/main/migrations/__pycache__/0001_initial.cpython-313.pyc 	modified:   tableshop/main/models.py 	modified:   tableshop/tableshop/__pycache__/settings.cpython-313.pyc 	modified:   tableshop/tableshop/__pycache__/urls.cpython-313.pyc 	modified:   tableshop/tableshop/settings.py 	modified:   tableshop/tableshop/urls.py

--- tableshop/main/models.py
+++ tableshop/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Kitchen(models.Model):
+    name = models.CharField("Название кухни", max_length=200)
+    facade_type = models.CharField("Вид фасада", max_length=200)
+    facade_image = models.ImageField("Изображение фасада", upload_to='facade_images/')
+    facade_coating = models.CharField("Покрытие фасадов", max_length=200)
+    size = models.CharField("Размер кухни", max_length=100)
+    island_size = models.CharField("Размеры кухонного острова", max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class KitchenImage(models.Model):
+    kitchen = models.ForeignKey(Kitchen, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField("Изображение кухни", upload_to='kitchen_images/')
+
+    def __str__(self):
+        return f"Фото для {self.kitchen.name}"
--- tableshop/tableshop/settings.py
+++ tableshop/tableshop/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'tableshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prfasad',        
+        'USER': 'postgres',     
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -116,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
